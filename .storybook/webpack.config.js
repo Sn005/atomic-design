a9fc96cba207ff86162e6862f604f8676869eafa
narihara <narihara@webnear.co.jp>
2018-05-22 11:14:09 +0900

    mod storyのwebpack設定変更

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 
 module.exports = (storybookBaseConfig, configType, defaultConfig) => {
+
   defaultConfig.module.rules.push({
     test: [/\.stories\.js$/, /index\.js$/],
     loaders: [require.resolve('@storybook/addon-storysource/loader')],
     include: [path.resolve(__dirname, '../src')],
     enforce: 'pre',
-  });
-
+  })
+  defaultConfig.resolve.alias['@components'] = path.resolve(__dirname, '../src/components/')
   return defaultConfig;
 }
