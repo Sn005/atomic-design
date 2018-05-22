ebcd5d8acbe59adbee6d64aff21c95c5c98247b0
narihara <narihara@webnear.co.jp>
2018-05-22 12:53:42 +0900

    create atom/img.vue

---
+++ src/components/atoms/Img.vue
@@ -0,0 +1,22 @@
+<template lang='pug'>
+  img(
+    ...props
+  )
+</template>
+<script>
+export default {
+  name: 'Img',
+  props: {
+    href: {
+      type: String,
+      required: true
+    },
+    width: {
+      type: String
+    },
+    height: {
+      type: String
+    }
+  }
+}
+</script>
