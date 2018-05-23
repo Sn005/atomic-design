43ab92c287eada61ce9fb0cb1766962c9dd33aad
narihara <narihara@webnear.co.jp>
2018-05-23 17:22:45 +0900

    create organisms/Notification.vue

---
+++ src/components/organisms/Notification.vue
@@ -0,0 +1,11 @@
+<template lang='pug'>
+</template>
+<script>
+import TrashCanIcon from '@components/atoms/Icons/TrashCanIcon.vue'
+import Balloon from '@components/atoms/Balloon.vue'
+export default {
+  name: 'Notification'
+}
+</script>
+<style scoped lang='scss'>
+</style>
