5e8831fcbee9d9ecd0959f94248dd2522ed72239
narihara <narihara@webnear.co.jp>
2018-05-24 18:46:18 +0900

    create HoverTipInteraction.vue

---
+++ src/components/atoms/HoverTipInteraction.vue
@@ -0,0 +1,36 @@
+<template lang='pug'>
+div.root
+  div.marker
+    slot( name="marker" )
+  div.tip
+    slot( name="tip" )
+</template>
+<script>
+export default {
+  name: 'HoverTipInteraction'
+}
+</script>
+<style scoped lang='scss'>
+.root{
+  position: relative;
+  &:hover{
+    > {
+      .tip{
+        display: inline-block;
+      }
+      .marker{
+        background-color: #f6f6f6;
+      }
+    }
+  }
+}
+.tip{
+  display: none;
+  position: absolute;
+  top: 0;
+  left: -50%;
+  transform: translate(-22%, -100%)translateY(-12px);
+  white-space: nowrap;
+}
+
+</style>
