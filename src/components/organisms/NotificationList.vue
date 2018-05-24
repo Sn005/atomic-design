d68789ba1e5f754465f5bd12731d7f3b891ab5ac
narihara <narihara@webnear.co.jp>
2018-05-24 18:01:45 +0900

    create NotificationList.vue

---
+++ src/components/organisms/NotificationList.vue
@@ -0,0 +1,31 @@
+<template lang='pug'>
+  notification.item(
+    v-for(program in programs)
+    :key="program.id"
+    :program="program"
+    @delete-action="action"
+  )
+</template>
+<script>
+import Notification from '@/components/organisms/Notification.vue'
+export default {
+  name: 'NotificationList',
+  components: {Notification},
+  props: {
+    programs: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped lang='scss'>
+.item{
+  &:hover{
+    background-color: #f6f6f6;
+    & + & {
+      border-top: 1px solid #ddd;
+    }
+  }
+}
+</style>
