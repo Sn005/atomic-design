b6100521bb128432c0e36c576ba712cfb77eb8fc
narihara <narihara@webnear.co.jp>
2018-05-25 16:41:55 +0900

    create AppTextBox.vue

---
+++ src/components/atoms/AppTextBox.vue
@@ -0,0 +1,19 @@
+<template lang='pug'>
+  input.textBox(
+    type="text"
+  )
+</template>
+<script>
+export default {
+  name: 'AppTextBox'
+}
+</script>
+<style scoped lang='scss'>
+.textBox{
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  color: #000;
+  font-size: .8rem;
+  padding: .5rem;
+}
+</style>
