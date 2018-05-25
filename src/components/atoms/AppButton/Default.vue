1f40d6e87c861829eeab67d99a2697521bfe3dca
narihara <narihara@webnear.co.jp>
2018-05-25 19:01:22 +0900

    up

--- src/components/atoms/AppButton/Default.vue
+++ src/components/atoms/AppButton/Default.vue
@@ -6,6 +6,20 @@ export default {
 }
 </script>
 <style scoped lang="scss">
+.button {
+  border-radius: 2px;
+  border-width: 0;
+  display: inline-block;
+  font-weight: 700;
+  font-size: 1rem;
+  line-height: 1;
+  padding: .8rem;
+  text-decoration: none;
+  transition: opacity .1s ease-out;
+  &:hover{
+    opacity: .7;
+  }
+}
 .status {
   background-color: inherit;
   border: 1px solid #ddd;
--- src/stories/atoms/buttons/index.js
+++ src/stories/atoms/buttons/index.js
@@ -1,25 +1,26 @@
 // src/stories/index.js
 import { storiesOf } from '@storybook/vue'
-import { withInfo } from 'storybook-addon-vue-info'
-import { withKnobs, text } from '@storybook/addon-knobs'
-// import { withNotes } from '@storybook/addon-notes'
-import { linkTo } from '@storybook/addon-links'
-import AppButton from '@/components/AppButton.vue'
-
-import readme from './README.md'
+import VueInfoAddon from 'storybook-addon-vue-info'
+import { withKnobs } from '@storybook/addon-knobs'
+import AppButton from '@/components/atoms/AppButton/Default.vue'
+import AppButtonPrimary from '@/components/atoms/AppButton/Primary.vue'
 
 storiesOf(`atoms/buttons`, module)
-  // .addDecorator(VueInfoAddon)
+  .addDecorator(VueInfoAddon)
   .addDecorator(withKnobs)
-  .add('default', withInfo(readme)(() => {
-    const label = text('Label', 'BUTTON')
+  .add('デフォルト', () => {
     return {
       components: { AppButton },
       template: `
-          <AppButton @click="handleClick">${label}</AppButton>
-      `,
-      methods: {
-        handleClick: linkTo('Atom/ButtonRadius')
-      }
+          <app-button>デフォルト</app-button>
+      `
     }
-  }))
+  })
+  .add('プライマリ', () => {
+    return {
+      components: { AppButtonPrimary },
+      template: `
+          <app-button-primary>プライマリ</app-button-primary>
+      `
+    }
+  })
