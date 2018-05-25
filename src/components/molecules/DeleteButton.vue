cd5ac36ccf62234c64868077c501afa35c621fff
narihara <narihara@webnear.co.jp>
2018-05-25 11:12:15 +0900

    mod refactor DeleteButton.vue

--- src/components/molecules/DeleteButton.vue
+++ src/components/molecules/DeleteButton.vue
@@ -1,34 +1,24 @@
 <template lang='pug'>
-  div.delete-button
-    app-trash-can-icon.delete-button__icon(clickable)
-    div.delete-button__balloon
+  hover-tip-interaction
+    app-trash-can-icon(
+      clickable
+      slot="root"
+    )
+    div(
+      slot="tip"
+    )
       app-balloon 削除する
 </template>
 <script>
 import AppTrashCanIcon from '@/components/atoms/AppIcon/TrashCanIcon.vue'
 import AppBalloon from '@/components/atoms/AppBalloon.vue'
+import HoverTipInteraction from '@/components/atoms/HoverTipInteraction.vue'
 export default {
   name: 'DeleteButton',
-  components: {AppTrashCanIcon, AppBalloon}
+  components: {
+    AppTrashCanIcon,
+    AppBalloon,
+    HoverTipInteraction
+  }
 }
 </script>
-<style scoped lang='scss'>
-.delete-button{
-  $root: #{&};
-  display: inline-block;
-  position: relative;
-  &__balloon{
-    display: none;
-    position: absolute;
-    top: 0;
-    left: -50%;
-    transform: translate(-22%, -100%)translateY(-12px);
-    white-space: nowrap;
-  }
-  &:hover{
-    #{$root}__balloon{
-      display: inline-block;
-    }
-  }
-}
-</style>
