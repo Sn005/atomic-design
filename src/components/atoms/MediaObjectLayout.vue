37b5b9347284cf0e068dd5fd03ba49709e81ef81
narihara <narihara@webnear.co.jp>
2018-05-25 13:09:12 +0900

    create MediaObjectLayput.vue

---
+++ src/components/atoms/MediaObjectLayout.vue
@@ -0,0 +1,29 @@
+<template lang='pug'>
+  component.container(
+    :is="tag"
+  )
+  div
+    slot(name="media")
+  div.body
+    slot(name="body")
+</template>
+<script>
+export default {
+  name: 'MediaObjectLayout',
+  props: {
+    tag: {
+      type: String,
+      default: 'div'
+    }
+  }
+}
+</script>
+<style scoped lang='scss'>
+.container{
+  display: flex;
+}
+.body{
+    flex: 1;
+    min-width: 0;
+}
+</style>
