219dc3b5ebc8e2395904e1b33c5697b122f8c87a
narihara <narihara@webnear.co.jp>
2018-05-25 11:39:25 +0900

    create AppBalloonTip.vue

---
+++ src/components/atoms/AppBalloonTIp.vue
@@ -0,0 +1,22 @@
+<template lang='pug'>
+  hover-tip-interaction
+    slot(
+      slot="marker"
+    )
+    app-balloon(
+      slot="tip"
+    ) slot(name="balloon")
+</template>
+<script>
+import AppBalloon from '@/components/atoms/AppBalloon.vue'
+import HoverTipInteraction from '@/components/atoms/HoverTipInteraction.vue'
+export default {
+  name: 'AppBalloonTip',
+  components: {
+    AppBalloon,
+    HoverTipInteraction
+  }
+}
+</script>
+<style scoped lang='scss'>
+</style>
