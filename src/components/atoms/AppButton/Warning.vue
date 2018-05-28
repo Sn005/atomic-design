ee55f6d6a2fd2fe886d41a4e19207a7d57697075
narihara <narihara@webnear.co.jp>
2018-05-28 13:22:06 +0900

    add sass-resources-loader

---
+++ src/components/atoms/AppButton/Warning.vue
@@ -0,0 +1,13 @@
+<script>
+import Presenter from './Presenter.vue'
+export default {
+  name: 'AppButtonWarning',
+  extends: Presenter
+}
+</script>
+<style scoped lang="scss" src="./style.scss">
+.status {
+  background-color: $color-warning;
+  color: #fff;
+}
+</style>
