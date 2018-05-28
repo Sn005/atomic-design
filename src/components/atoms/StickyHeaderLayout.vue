f608aa3a3da5fe5371c1132ff22b66d4e07dbe2b
narihara <narihara@webnear.co.jp>
2018-05-28 17:28:38 +0900

    create StickyHeaderLayout components

---
+++ src/components/atoms/StickyHeaderLayout.vue
@@ -0,0 +1,34 @@
+<template lang='pug'>
+  component.state(
+    :is="tag"
+  )
+    div.sticky
+      slot(name="sticky") 固定部分componentが入ります
+    div.sticky
+      slot(name="body") body部分componentが入ります
+</template>
+<script>
+export default {
+  name: 'StickyHeaderLayout',
+  props: {
+    tag: {
+      type: String,
+      default: 'div',
+      validator (val) {
+        return ['div', 'section'].includes(val)
+      }
+    }
+  }
+}
+</script>
+<style scoped lang='scss'>
+.sticky {
+  position: fixed;
+  width: 100%;
+}
+
+.body {
+  min-height: 100vh;
+}
+
+</style>
