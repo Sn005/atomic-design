9108350e4cea8d8c4c71de56dc2a84a2dbee343f
narihara <narihara@webnear.co.jp>
2018-05-29 11:55:59 +0900

    create Header.vue

---
+++ src/components/organisms/Header.vue
@@ -0,0 +1,17 @@
+<template lang='pug'>
+  div.root
+</template>
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+<style scoped lang='scss'>
+@import "../../assets/styles/_properties.scss";
+
+.root {
+  padding: calc($space * 2);
+  background-color: $color-header;
+  color: $color-text-outlined;
+}
+</style>
