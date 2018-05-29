d6c65c8d676d5f0607221ec0fd139379aa3c96fe
narihara <narihara@webnear.co.jp>
2018-05-29 12:22:25 +0900

    create Navitation.vue

---
+++ src/components/molecules/Navigation.vue
@@ -0,0 +1,52 @@
+<template lang='pug'>
+  nav
+    ul.list(
+      v-for="(item, index) in items"
+      :key="index"
+    )
+      li.item
+        a.link(
+          v-if="!item.curren"
+          :href="item.href"
+        ) {{ item.label }}
+        span.link.curren(
+          v-else
+        ) {{ item.label }}
+</template>
+<script>
+export default {
+  name: 'Navigation',
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped lang='scss'>
+@import "../../assets/styles/_properties.scss";
+
+.list {
+  display: flex;
+  flex-direction: rows;
+  font-size: .8rem;
+}
+.item + .item {
+  padding: 0 0 0 2em;
+}
+
+.link {
+  color: #fff;
+  padding: calc($space * 0.5) 0;
+  @at-root .current{
+    border-color: $color-primary;
+    border-style: solid;
+    border-width: 0 0 .1rem 0;
+    color: #fff;
+  }
+  &:hover{
+    @extend .current;
+  }
+}
+</style>
