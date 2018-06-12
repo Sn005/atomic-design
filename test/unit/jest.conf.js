fddf2d6cb30fa1c43fc5f805d9fa8e131f2476e3
snasna <wisperinthedustbox@gmail.com>
2018-06-12 23:25:26 +0900

    test追加

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -1,27 +1,27 @@
 const path = require('path')
 
 module.exports = {
-  globals: {
-    __TRANSFORM_HTML__: true
-  },
+  // globals: {
+  //   __TRANSFORM_HTML__: true
+  // },
   rootDir: path.resolve(__dirname, '../../'),
-  moduleFileExtensions: ['vue', 'js', 'jsx', 'json', 'node'],
-  // moduleFileExtensions: [
-  //   'js',
-  //   'json',
-  //   'vue'
-  // ],
+  // moduleFileExtensions: ['vue', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue'
+  ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
-    '.*\\.(vue)$': '<rootDir>/node_modules/jest-vue-preprocessor'
-    // '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+    // '.*\\.(vue)$': '<rootDir>/node_modules/jest-vue-preprocessor'
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
   },
   testPathIgnorePatterns: [
-    '/node_modules/(?!(@storybook/.*\\.vue$))'
-    // '<rootDir>/test/e2e'
+    // '/node_modules/(?!(@storybook/.*\\.vue$))'
+    '<rootDir>/test/e2e'
   ],
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
--- test/unit/specs/atoms/AppHeading.spec.js
+++ test/unit/specs/atoms/AppHeading.spec.js
@@ -1,11 +1,19 @@
-import Vue from 'vue'
+import { shallowMount } from '@vue/test-utils'
 import AppHeading from '@/components/atoms/AppHeading/Default.vue'
 
 describe('AppHeading.vue', () => {
   it('見た目レベルが指定されていない時、見出しレベルに合わせる', () => {
-    const Constructor = Vue.extend(AppHeading)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .toEqual('Welcome to Your Vue.js App')
+    const wrapper = shallowMount(AppHeading)
+    expect(wrapper.contains('h1')).toBe(true)
+  })
+  it('見た目レベルが指定されているときは見出しレベルを合わせない', () => {
+    const props = {
+      propsData: {
+        level: 1,
+        visualLevel: 2
+      }
+    }
+    const wrapper = shallowMount(AppHeading, props)
+    expect(wrapper.props().visualLevel).toBe(2)
   })
 })
